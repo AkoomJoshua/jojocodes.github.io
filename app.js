8edb1d304af4a80ed8a2f15dc17e6c7d504a2b62
Joshua Akoom <sandiego2643@gmail.com>
2022-03-28 16:14:57 +0100

    new files added
    i added new files to my portfolio website

---
+++ app.js
@@ -0,0 +1,14 @@
+function validateForm() {
+    var x = document.forms["myForm"]["fullname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+      
+    }
+    else (x == "this") 
+        return this;
+    }
+  }
+  function slideSho() {
+    console.log("slideShork working")
+  }
